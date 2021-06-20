968695ed4da360ee876638d3809f5d049ccc354b
JuanR <juan.restrepo81@udea.edu.co>
2021-06-20 13:18:41 -0500

    first commit

--- vars/getConfig.groovy
+++ vars/getConfig.groovy
@@ -28,3 +28,16 @@ def call(givenConfig = [:]) {
   println "Effective config: " + effectiveConfig
   return effectiveConfig
 }
+
+def merge(Map lhs, Map rhs) {
+    return rhs.inject(lhs.clone()) { map, entry ->
+        if (map[entry.key] instanceof Map && entry.value instanceof Map) {
+            map[entry.key] = merge(map[entry.key], entry.value)
+        } else if (map[entry.key] instanceof Collection && entry.value instanceof Collection) {
+            map[entry.key] += entry.value
+        } else {
+            map[entry.key] = entry.value
+        }
+        return map
+    }
+}
