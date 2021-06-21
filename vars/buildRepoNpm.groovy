951b7d4221dca5fd1bf2c28883a515e17138301a
JuanR <juan.restrepo81@udea.edu.co>
2021-06-20 21:20:49 -0500

    first commit

--- vars/buildRepoNpm.groovy
+++ vars/buildRepoNpm.groovy
@@ -1,25 +1,50 @@
 def call(buildPlan = [:]) {
-  node(buildPlan.jenkinsNode) {
-    stage("Install") {
-      sh "npm install"
+
+    pipeline {
+        agent any
+        stages {
+            stage ("Git pull"){
+                steps{
+                    git branch: 'master', url: 'https://github.com/juancarestre/angular_get_started.git'
+                    sh 'ls'
+                }
+            }
+            stage("build"){
+                steps{
+                    sh 'npm install'
+                    //sh 'npm run test'
+                    sh 'npm run build'
+                }
+            }      
+            stage("deploy"){
+                steps{
+                    echo "aws s3 ls"
+                }
+            }
+        }
     }
-    stage("Build") {
-      sh "npm run build"
-    }
-    if (buildPlan.npm.tslint) {
-      stage("TSlint") {
-        sh "npm run tslint"
-      }
-    }
-    if (buildPlan.npm.eslint) {
-      stage("ESlint") {
-        sh "npm run eslint"
-      }
-    }
-    if (buildPlan.npm.cypress) {
-      stage("Cypress") {
-        sh "npm run e2e:cypress"
-      }
-    }
-  }
+
+//   node(buildPlan.jenkinsNode) {
+//     stage("Install") {
+//       sh "npm install"
+//     }
+//     stage("Build") {
+//       sh "npm run build"
+//     }
+//     if (buildPlan.npm.tslint) {
+//       stage("TSlint") {
+//         sh "npm run tslint"
+//       }
+//     }
+//     if (buildPlan.npm.eslint) {
+//       stage("ESlint") {
+//         sh "npm run eslint"
+//       }
+//     }
+//     if (buildPlan.npm.cypress) {
+//       stage("Cypress") {
+//         sh "npm run e2e:cypress"
+//       }
+//     }
+//   }
 }
